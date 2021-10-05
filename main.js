76228bb9f6a37b8f221f696c7593d39adec423d2
Tiana <Tiana.marie013@gmail.com>
2021-10-05 17:17:26 -0600

    tests

--- main.js
+++ main.js
@@ -1,5 +1,42 @@
-const firstName = "Tiana";
-const age = 35;
+// const firstName = "Tiana";
+// const age = 35;
+// const s = "hello world"
 
 
-console.log(`My name is ${firstName} and I am ${age}`);
+// // console.log(`My name is ${firstName} and I am ${age}`);
+// console.log(s)
+// console.log(s.toUpperCase);
+// console.log(s.substring(0,5));
+// console.log(s.length);
+// console.log(s.split(''));
+
+// const fruit = ['apple', ' pear', 'pineapple', 'orange']
+
+// fruit[3]='grapes';
+// fruit.push('mangos');
+// fruit.pop();
+
+// console.log(Array.isArray('fruit'));
+// console.log(fruit.indexOf('grapes'))
+// console.log(fruit);
+
+// const person = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     age: 30,
+//     hobbies:['music','dance','paint'],
+//     address: {
+//         street: '5 Tejon st',
+//         city: 'Denver',
+//         state: 'CO'
+//     }
+// }
+
+// // console.log(person.address.street);
+
+// const {firstName, age} = person
+// console.log(firstName)
+
+// person.email = " john@gmail.com";
+
+// console.log( person)

012e7c5313389fa3ff33dff712a56de826d55d72
Tiana <Tiana.marie013@gmail.com>
2021-10-06 07:21:41 -0600

    prototypes

--- main.js
+++ main.js
@@ -151,20 +151,25 @@ function Person(firstName, lastName, dob){
     this.firstName = firstName;
     this.lastName = lastName;
     this.dob = new Date(dob);
-    this.getBirthYear = function(){
-        return this.dob.getFullYear();
-    }
-    this.getFullName = function(){
-        return `${this.firstName} ${this.lastName}`
-    }
 
+
+};
+
+Person.prototype.getBirthYear = function() {
+    return this.dob.getFullYear();
 }
+Person.prototype.getFullName = function() {
+    return `${this.firstName} ${this.lastName}`
+}
+
+
 // Instantiate object
 const person1 = new Person('John', 'Doe', '07/08/1986');
 const person2 = new Person('Paul', 'Ridge', '6/6/2020');
 
-console.log(person1.getBirthYear())
-console.log(person2.getFullName())
+console.log(person1.getBirthYear());
+console.log(person2.getFullName());
+console.log(person1);
 
 
 

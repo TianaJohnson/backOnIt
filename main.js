1dd6834caa12f6685e7f3743ec81e3b8e75b8fda
Tiana <Tiana.marie013@gmail.com>
2021-10-06 19:37:58 -0600

    Dom selection

--- main.js
+++ main.js
@@ -191,13 +191,13 @@
 
 //single element selectors
 
-console.log(document.getElementById('my-form'));
-console.log(document.querySelector('.container'));
-
+// console.log(document.getElementById('my-form'));
+// console.log(document.querySelector('h1'));
 
 //multi element 
 
-
+// console.log(document.querySelectorAll('.item'))
+// console.log(document.getElementsByClassName('item'))
 
 
 

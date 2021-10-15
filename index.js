0cf470826a99e6889846cd7400ed3dbe6140c226
Tiana <Tiana.marie013@gmail.com>
2021-10-15 10:29:01 -0600

    more node practice

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //Node Crash Course
 console.log('Hello from node.js');
+//import Person from './person' will only work with babble (ES6)
 
-const Person = require('./person')
+const Person = require('./person')   ///common js
 const person1 = new Person('Jane', 'Doe');
 
 person1.greeting();
--- refrence/os_demo.js
+++ refrence/os_demo.js
@@ -1,4 +1,22 @@
-const os = require('os');
+const os = require("os");
 
 //Platform
-console.log(os.platform());
+// console.log(os.platform());
+
+// //CPU arch
+// console.log(os.arch());
+
+// //CPU Core info
+// console.log(os.cpus());
+
+// Free memory
+console.log(os.freemem())
+
+//Total memory
+console.log(os.totalmem())
+
+// Home dir
+console.log(os.homedir());
+
+// uptime
+console.log(os.uptime());

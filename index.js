159b27770438d28f70bce185322fbc089eb40a27
Tiana <Tiana.marie013@gmail.com>
2021-10-18 20:05:17 -0600

    JSON

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ const server = http.createServer((req, res) => {
     })
 }
 
-    if(req.url ==='/about'){
-        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
-        if (err) throw err;
-        res.writeHead(200, { "Content-Type": "text/html"});
-        res.end(content);
-    })
-}
+    if(req.url ==='/api/users'){
+        const users = [
+            {firstName: 'Bob Dough', age: 45},
+            {firstName: 'Tom John', age: 1}
+        ];
+        res.writeHead(200, { "Content-Type": "application/json"});
+        res.end(JSON.stringify(users));
+ }
 });
 const PORT = process.env.PORT || 5000;
 
